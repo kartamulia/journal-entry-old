c5253d1c9cdf80e5e664f68c2ee61a340200879f
Muhammad Yamin <kartamulia@outlook.com>
2018-03-22 03:27:34 +0700

    Add account repository.

---
+++ Kartamulia.Accounting.Repositories/AccountRepository.cs
@@ -0,0 +1,48 @@
+﻿using Kartamulia.Accounting.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kartamulia.Accounting.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<IEnumerable<Account>> GetAccounts(CancellationToken cancellationToken);
+
+        Task<Account> GetAccount(string accountNumber, CancellationToken cancellationToken);
+    }
+
+    public class AccountRepository
+    {
+        private static List<Account> _accounts = new List<Account>()
+        {
+
+        };
+
+        #region ctor
+
+        public AccountRepository()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<IEnumerable<Account>> GetAccounts(CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_accounts);
+        } 
+
+        public async Task<Account> GetAccount(string accountNumber, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_accounts.FirstOrDefault(x => x.Number == accountNumber));
+        }
+
+        #endregion
+    }
+}
