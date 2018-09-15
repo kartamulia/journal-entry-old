e5504e3cdcce83134d70f3115436ead934127466
m.yamin <m.yamin@outlook.com>
2018-09-16 03:25:41 +0700

    Add account.

---
+++ Asp Net Core Mvc Client/Kartamulia.Accounting.NetCoreMvcClient/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kartamulia.Accounting.NetCoreMvcClient.Models
+{
+    public class Account
+    {
+        #region ctor
+
+        public Account()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        #endregion
+    }
+}
