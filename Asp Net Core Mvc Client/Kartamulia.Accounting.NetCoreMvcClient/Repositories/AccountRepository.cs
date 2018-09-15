6127fceb6a47e3dfa02c8a4deecbd838894d0eee
m.yamin <m.yamin@outlook.com>
2018-09-16 03:26:31 +0700

    Add account repository.

---
+++ Asp Net Core Mvc Client/Kartamulia.Accounting.NetCoreMvcClient/Repositories/AccountRepository.cs
@@ -0,0 +1,61 @@
+﻿using Kartamulia.Accounting.NetCoreMvcClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Kartamulia.Accounting.NetCoreMvcClient.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<List<Account>> GetAccountsAsync();
+    }
+
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly string baseAddress = "http://localhost:65453/";
+
+        #region ctor
+
+        public AccountRepository()
+            : base()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<List<Account>> GetAccountsAsync()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseAddress);
+                    string requestUri = "api/accounts";
+                    var response = await client.GetAsync(requestUri);
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return await response.Content.ReadAsAsync<List<Account>>();
+                    }
+                    else
+                    {
+                        throw new Exception(response.StatusCode.ToString());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+}
