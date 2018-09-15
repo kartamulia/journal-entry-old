aba4af1c78ad880bae5b11dd62be818e2dfdea7b
m.yamin <m.yamin@outlook.com>
2018-09-16 03:26:11 +0700

    Add general journal.

---
+++ Asp Net Core Mvc Client/Kartamulia.Accounting.NetCoreMvcClient/Models/GeneralJournal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kartamulia.Accounting.NetCoreMvcClient.Models
+{
+    public class GeneralJournal : Journal
+    {
+        #region ctor
+
+        public GeneralJournal()
+            : base()
+        {
+        }
+
+        #endregion
+    }
+}
