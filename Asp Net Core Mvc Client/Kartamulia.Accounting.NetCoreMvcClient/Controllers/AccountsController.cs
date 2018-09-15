45942decf354a5c316d535b035a1f4b6c574ae91
m.yamin <m.yamin@outlook.com>
2018-09-16 03:25:05 +0700

    Add accounts controller.

---
+++ Asp Net Core Mvc Client/Kartamulia.Accounting.NetCoreMvcClient/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kartamulia.Accounting.NetCoreMvcClient.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kartamulia.Accounting.NetCoreMvcClient.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountsController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var accounts = await _accountRepository.GetAccountsAsync();
+            return View(accounts);
+        }
+    }
+}
